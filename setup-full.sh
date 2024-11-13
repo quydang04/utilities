d4990acad6fff8b9822a90bf1e9e4141a022a298
quydang <81231812+quydang04@users.noreply.github.com>
2024-11-13 10:39:08 +0700

    Update setup-full.sh

--- setup-full.sh
+++ setup-full.sh
@@ -15,6 +15,9 @@ sudo apt full-upgrade -y
 # Perform dist-upgrade
 sudo apt dist-upgrade -y
 
+#Remove old packages
+sudo apt autoremove -y 
+
 #Setup swap
 
 # Kiểm tra nếu người dùng là root
@@ -53,30 +56,6 @@ exit 0
 
 # Install AdguardHome
 curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
-
-#Setup DNS
-#!/bin/bash
-
-# Tạo thư mục /etc/systemd/resolved.conf.d nếu chưa tồn tại
-sudo mkdir -p /etc/systemd/resolved.conf.d
-
-# Tạo file adguardhome.conf với nội dung cấu hình
-sudo bash -c 'cat <<EOF > /etc/systemd/resolved.conf.d/adguardhome.conf
-[Resolve]
-DNS=127.0.0.1
-DNSStubListener=no
-EOF'
-
-# Sao lưu file resolv.conf hiện tại
-sudo mv /etc/resolv.conf /etc/resolv.conf.backup
-
-# Tạo liên kết tượng trưng (symlink) đến file resolv.conf mới
-sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
-# Khởi động lại dịch vụ systemd-resolved
-sudo systemctl reload-or-restart systemd-resolved
-
-echo "Đã thực hiện các thay đổi thành công."
 
 #Setup TinyCP
 sudo apt install apt-transport-https dirmngr gnupg ca-certificates -y
